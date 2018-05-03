a4613b710c798bc8bf670751604734bb5d8b0349
Salma Suliman <sahaja@salmas-macbook-air.local>
2018-05-03 14:25:55 -0700

    Initial Commit

---
+++ deadReckoning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  deadReckoning
+//
+//  Created by Salma Suliman on 5/3/18.
+//  Copyright © 2018 Echelon Front. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
